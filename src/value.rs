c9a5fde85c7f876f2e19d31ec3482b0aca826b5b
Enitoni <5295397+Enitoni@users.noreply.github.com>
2023-06-15 21:05:56 +0200

    impl From for Ident

--- src/value.rs
+++ src/value.rs
@@ -15,6 +15,18 @@ pub enum Ident<'a> {
     Record(Thing),
 }
 
+impl From<Thing> for Ident<'_> {
+    fn from(value: Thing) -> Self {
+        Ident::Record(value)
+    }
+}
+
+impl<'a> From<&'a str> for Ident<'a> {
+    fn from(value: &'a str) -> Self {
+        Ident::Table(value)
+    }
+}
+
 pub type BoxedValue<'a> = Box<dyn ValueLike + 'a>;
 pub trait ValueLike: erased_serde::Serialize + Debug + Send {}
 impl<T> ValueLike for T where T: erased_serde::Serialize + Debug + Send {}
